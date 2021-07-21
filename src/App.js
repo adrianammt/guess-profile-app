5419c094e90332164e9d399079d9cee3e689cae3
adrianammt <amorat@gmx.de>
2021-07-21 23:52:29 +0200

    Add structure and css

--- src/App.js
+++ src/App.js
@@ -2,7 +2,33 @@ import "./App.css";
 import { Switch, Route, NavLink } from "react-router-dom";
 
 function App() {
-  return <div className="App">Hello world</div>;
+  return (
+    <div className="App">
+      <header className="App-Header">
+        <h1>Guess your ""</h1>
+      </header>
+      <main className="App-Content">
+        <form className="App-Form">
+          <input type="text" id="nameInput" name="nameInput" />
+          <button className="App-Form__Submit" type="submit">
+            Submit
+          </button>
+        </form>
+        <section>
+          <p>Age:</p>
+          <p>Nationality:</p>
+          <p>Gender:</p>
+        </section>
+      </main>
+      <footer className="App-Footer">
+        <div className="App-Footer__Options">
+          <button>Age</button>
+          <button>Nationality</button>
+          <button>Gender</button>
+        </div>
+      </footer>
+    </div>
+  );
 }
 
 export default App;
